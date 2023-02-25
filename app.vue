d5fbf2355836170d01e14022beb37e260f8f7aed
reky senjaya <rekysenjaya@gmail.com>
2023-02-25 07:11:19 +0700

    create route index, detail

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div class="min-h-screen w-screen bg-[#1E232B] text-[#E5E5E5]">
+		<NuxtLayout>
+			<NuxtLoadingIndicator />
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
 </template>
---
+++ pages/detail.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+	<div>
+detail boos
+	</div>
+</template>
