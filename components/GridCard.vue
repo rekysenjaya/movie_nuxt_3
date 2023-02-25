ef1ce2f8a375009b68bf268c4d9bd0e47137b92d
reky senjaya <rekysenjaya@gmail.com>
2023-02-25 10:55:46 +0700

    card title

---
+++ components/GridCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+const { title } = defineProps(['title'])
+const sortBy = ref('popularity')
+
+const clickOrderBy = (val) => {
+	sortBy.value = val
+}
+</script>
+
+<template>
+	<div>
+		<div class="bg-[#292e35] pt-16 pb-48">
+			<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+				<div class="mb-2 h-1 bg-[#E74C3C] w-[112px]" />
+				<div class="flex justify-between">
+					<h3 class="text-2xl font-semibold">{{ title }}</h3>
+					<div class="flex flex-row">
+						<button v-for="val in ['popularity', 'Release Date']"
+							class="p-1 w-28 text-center rounded-full border-2 hover:font-semibold text-sm m-1"
+							v-on:click="clickOrderBy(val)"
+							:class="sortBy === val ? 'bg-[#fc0d39] border-[#fc0d39] hover:bg-red-700 hover:border-2' : 'bg-[#1e232a] border-[#1e232a] hover:bg-[#23272c]'">{{
+								val }}</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 -mt-44">
+			<div class="grid sm:grid-cols-4 lg:grid-cols-6 gap-4 gap-y-8">
+				<CardMovie v-for="slide in 10" :key="slide" :movie="{ key: slide }" />
+			</div>
+		</div>
+	</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,4 @@
 <template>
 	<Carousel3D />
-	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-		<div class="grid sm:grid-cols-4 lg:grid-cols-6 gap-4 gap-y-8">
-			<CardMovie v-for="slide in 10" :key="slide" :movie="{ key: slide }" />
-		</div>
-	</div>
+	<GridCard :title="'Discover Movies'" />
 </template>
