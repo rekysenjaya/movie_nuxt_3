c8e1a3a344a283a63d5dc51b36181e6d4ff3795a
reky senjaya <rekysenjaya@gmail.com>
2023-02-25 09:24:09 +0700

    description card carousel

--- components/CardMovie/CarouselDesc.vue
+++ components/CardMovie/CarouselDesc.vue
@@ -1,11 +1,16 @@
 <template>
-	<div class="p-5 h-[18rem] text-left overflow-hidden">
-		<div class="flex flex-row text-lg">
+	<div class="p-5 h-[18rem] text-left font-normal overflow-hidden grid gap-2">
+		<div class="flex flex-row items-center text-lg">
 			<img class="h-4 mr-1 w-auto" src="/start.png" alt="start" />
 			<span>7.0</span>
 		</div>
-		<h3 class="text-xl uppercase">Space Sweepers</h3>
-		<div class="text-sm desc">
+		<h3 class="text-xl font-bold uppercase">Space Sweepers</h3>
+		<div class="flex flex-row items-center">
+			<div class="text-base">2021</div>
+			<div class="bg-[#808080] h-1.5 w-1.5 m-1 rounded-full" />
+			<div class="text-base">Sci-Fi</div>
+		</div>
+		<div class="text-sm">
 			When the crew of a space junk collector ship called The Victory discovers a humanoid robot named Dorothy that's
 			known to be a weapon of mass destruction, they get involved in a risky business deal which puts their lives at
 			stake.
