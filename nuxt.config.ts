8cc57532b2d70d116ce33b07e417dce757b6ea20
reky senjaya <rekysenjaya@gmail.com>
2023-02-25 07:10:41 +0700

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'Movies',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width-device-width, initial-scale-1' }
+			]
+		}
+	},
 
+	modules: [
+		'@nuxtjs/tailwindcss'
+	]
 })
