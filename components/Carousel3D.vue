c4ba418b5c27e7ac9d2cccd8df0297cba88ceb56
reky senjaya <rekysenjaya@gmail.com>
2023-02-27 15:11:25 +0700

    style navigation

--- components/Carousel3D.vue
+++ components/Carousel3D.vue
@@ -1,9 +1,9 @@
 <script setup>
 import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { Carousel, Slide, Pagination } from 'vue3-carousel'
 import constants from '~~/constants';
-
-const { loading, list, getList } = useAutocompleteSearch()
+const currentSlide = ref(0)
+const { list, getList } = useAutocompleteSearch()
 
 onMounted(() => {
 	if (!list.value.length) {
@@ -11,18 +11,22 @@ onMounted(() => {
 	}
 })
 
+const slideTo = (val) => currentSlide.value = val
+
 </script>
 
 <template>
 	<div class="pt-5 pb-5">
-		<Carousel :itemsToShow="3" :wrapAround="true" :transition="500">
+		<Carousel :itemsToShow="3" :wrapAround="true" :transition="500" v-model="currentSlide">
 			<Slide v-for="movie in list" class="pl-5 pr-5">
 				<CardMovieCarousel :movie="{ ...movie, rate: constants.getRndInteger(3.0, 5) }" />
 			</Slide>
-			<template #addons>
-				<pagination />
-			</template>
 		</carousel>
+
+		<div class="flex flex-row items-center justify-center">
+			<div v-for="(slide, index) in list" class="h-3 w-3 m-1 rounded-full cursor-pointer"
+				:class="currentSlide === index ? 'w-16 bg-[#E74C3C] ' : 'bg-[#808080]'" @click="slideTo(index)" />
+		</div>
 	</div>
 </template>
 
